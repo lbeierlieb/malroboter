892ea4c83faf83f570afce415207d2a3d94a3abd
Lukas Beierlieb <lukas.beierlieb@uni-wuerzburg.de>
2022-12-25 23:57:45 +0100

    Some box moving on its own

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,34 @@
 
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Movable {
+    start_pos : Vec3,
+    end_pos : Vec3,
+    t : f32,
+    dt : f32
+}
+
+impl Movable {
+    fn update(&mut self) {
+        self.t += self.dt;
+
+        if (self.t > 1.0 && self.dt > 0.0)
+        || (self.t < 0.0 && self.dt < 0.0){
+            self.dt *= -1.0;
+        }
+    }
+
+    fn current(&self) -> Vec3 {
+        self.start_pos + self.t * (self.end_pos - self.start_pos)
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
+        .add_system(moving)
         .run();
 }
 
@@ -22,12 +46,12 @@ fn setup(
         ..default()
     });
     // cube
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+    commands.spawn((PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Box::new(1.0, 2.0, 1.0))),
         material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
         ..default()
-    });
+    }, Movable { start_pos: Vec3::new(0.0, 1.0, 0.0), end_pos: Vec3::new(1.0, 1.0, 2.0), t: 0.0, dt: 0.001 }));
     // light
     commands.spawn(PointLightBundle {
         point_light: PointLight {
@@ -40,8 +64,21 @@ fn setup(
     });
     // camera
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(-6.0, 2.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
+}
+
+fn moving (
+    keyboard: Res<Input<KeyCode>>,
+    mut movables: Query<(&mut Transform,&mut Movable)>,
+) {
+    if keyboard.just_pressed(KeyCode::Space) { return; }
+
+    for m in &mut movables {
+        let (mut player_position, mut movable) = m;
+        movable.update();
+        player_position.translation = movable.current();
+    }
 }
 
